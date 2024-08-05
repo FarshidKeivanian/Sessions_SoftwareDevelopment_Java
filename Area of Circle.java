83fcdcb9e157214301f301e233bed01281c19bcd
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-08-05 19:36:00 +1000

    Add files via upload

---
+++ Area of Circle.java
@@ -0,0 +1,20 @@
+package area_of_circle;
+
+public class circle {
+
+	public static void main(String[] args) {
+	    double radius; // Declare radius
+	    double area; // Declare area
+
+	    // Assign a radius
+	    radius = 20; // New value is radius
+
+	    // Compute area
+	    area = radius * radius * 3.14159;
+
+	    // Display results
+	    System.out.println("The area for the circle of radius " +
+	      radius + " is " + area);
+	}
+
+}

25e8ba231219d31fbd3880568f9af6126e46b2a1
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-08-13 14:19:40 +1000

    Update BooleanDivisibilityTester.java

--- BooleanDivisibilityTester.java
+++ BooleanDivisibilityTester.java
@@ -15,9 +15,6 @@ public class BooleanDivisibilityTester {
 	    if (number % 2 == 0 && number % 3 == 0)
 	      System.out.println(number + " is divisible by 2 and 3.");
 
-	    if (number % 2 == 0 || number % 3 == 0)
-	      System.out.println(number + " is divisible by 2 or 3.");
-
 	    if (number % 2 == 0 ^ number % 3 == 0)
 	      System.out.println(number + 
 	        " divisible by 2 or 3, but not both.");

eaeea6259c7bda5aed65ad399a4c786588e86960
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-09-20 07:46:16 +1000

    Update CommonLetters.java

--- CommonLetters.java
+++ CommonLetters.java
@@ -1,42 +1,43 @@
-package digitCount;
+package commonLetters;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class DigitCount {
-
-    // Method to count occurrences of each digit (0-9) in the number
-    public static int[] count(long num) {
-        int[] digitCounts = new int[10]; // Array to hold counts for digits 0-9
-
-        // Convert the long number to a string
-        String numStr = Long.toString(num);
-
-        // Loop through each character in the string
-        for (int i = 0; i < numStr.length(); i++) {
-            char ch = numStr.charAt(i); // Get each character
-            int digit = Character.getNumericValue(ch); // Convert char to int
-            digitCounts[digit]++; // Increment the count for this digit
-        }
-        return digitCounts;
-    }
+public class CommonLetters {
 
     public static void main(String[] args) {
-        // Create a scanner object to take input from the user
+        // Create a Scanner object to take input from the user
         Scanner input = new Scanner(System.in);
-        
-        // Prompt the user to enter a number
-        System.out.print("Enter a number: ");
-        long number = input.nextLong(); // Read long number from input
 
-        // Call the count method to get digit counts
-        int[] counts = count(number);
-
-        // Display the result for each digit
-        for (int i = 0; i < counts.length; i++) {
-            if (counts[i] > 0) {
-                System.out.println("Digit " + i + " occurs " + counts[i] + " time(s)");
-            }
+        // Input for the first list of characters
+        System.out.print("Enter the number of elements for list1: ");
+        int list1Size = input.nextInt();
+        System.out.print("Enter list1: ");
+        ArrayList<Character> list1 = new ArrayList<>();
+        for (int i = 0; i < list1Size; i++) {
+            String str = input.next();  // read individual characters as string
+            list1.add(str.charAt(0));   // convert string to char and add to list
         }
-        input.close();
+
+        // Input for the second list of characters
+        System.out.print("Enter the number of elements for list2: ");
+        int list2Size = input.nextInt();
+        System.out.print("Enter list2: ");
+        ArrayList<Character> list2 = new ArrayList<>();
+        for (int i = 0; i < list2Size; i++) {
+            String str = input.next();
+            list2.add(str.charAt(0));   // convert string to char and add to list
+        }
+
+        // Find and retain common elements between list1 and list2
+        list1.retainAll(list2);
+
+        // Display the common elements
+        System.out.print("The common elements are: ");
+        for (Character ch : list1) {
+            System.out.print(ch + " ");
+        }
+
+        input.close();  // Close the scanner
     }
 }

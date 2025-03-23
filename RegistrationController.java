58956ae319ba6fdf93900b532459d69c64c5be46
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2025-03-24 05:23:55 +1100

    Add files via upload

--- RegistrationController.java
+++ RegistrationController.java
@@ -1,14 +1,47 @@
-import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ComboBox;
+package com.example.javafxregistrationform;
 
-public class RegistrationController {
-    @FXML private TextField nameField;
-    @FXML private TextField emailField;
-    @FXML private ComboBox<String> countryBox;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegistrationController implements Initializable {
 
     @FXML
-    public void registerUser() {
-        System.out.println("User Registered: " + nameField.getText() + " - " + emailField.getText());
+    private TextField nameField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private ComboBox<String> countryCombo;
+
+    @FXML
+    private RadioButton maleRadio;
+
+    @FXML
+    private RadioButton femaleRadio;
+
+    @FXML
+    private Button registerButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        countryCombo.getItems().addAll("Australia", "US", "UK", "Canada", "India");
+    }
+
+    @FXML
+    private void handleRegister() {
+        String name = nameField.getText();
+        String email = emailField.getText();
+        String country = countryCombo.getValue();
+        String gender = maleRadio.isSelected() ? "Male" : "Female";
+
+        System.out.println("Registered: " + name + " | " + email + " | " + gender + " | " + country);
     }
 }

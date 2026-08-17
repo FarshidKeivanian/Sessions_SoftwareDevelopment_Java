21578d408ffa5f56b62189c01891f928d86b9c1f
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2026-08-17 21:26:30 +1000

    Add files via upload

---
+++ Lab2_Launcher.java
@@ -0,0 +1,10 @@
+package org.example.itec313_lab2_javafx;
+
+import javafx.application.Application;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        Application.launch(FourClocks.class, args);
+    }
+}
